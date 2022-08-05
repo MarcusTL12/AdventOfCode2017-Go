d4b09a32c3e1d640631abba9e82ac8c946676fb8
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-08-05 16:56:16 +0200

    Day 1

--- days/day1.go
+++ days/day1.go
@@ -1,7 +1,45 @@
 package days
 
-import "fmt"
+import (
+	"os"
+)
 
-func Part1() {
-	fmt.Println("Day 1 part 1")
+var Day1 = [2]func(){part1, part2}
+
+func part1() {
+	inp, err := os.ReadFile("input/day1/input")
+	if err != nil {
+		panic(err)
+	}
+
+	ans := 0
+	if inp[0] == inp[len(inp)-1] {
+		ans = int(inp[0])
+	}
+
+	for i := 0; i < len(inp)-1; i++ {
+		if inp[i] == inp[i+1] {
+			ans += int(inp[i]) - '0'
+		}
+	}
+
+	println(ans)
+}
+
+func part2() {
+	inp, err := os.ReadFile("input/day1/input")
+	if err != nil {
+		panic(err)
+	}
+
+	ans := 0
+
+	for i, j := 0, len(inp)/2; j < len(inp); i++ {
+		if inp[i] == inp[j] {
+			ans += 2 * (int(inp[i]) - '0')
+		}
+		j++
+	}
+
+	println(ans)
 }
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "aoc2017/days"
+import (
+	"aoc2017/days"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+var funcs = [][2]func(){days.Day1}
 
 func main() {
-	days.Part1()
+	day, day_err := strconv.ParseInt(os.Args[1], 10, 64)
+	part, part_err := strconv.ParseInt(os.Args[2], 10, 64)
+
+	if day_err != nil {
+		panic(day_err)
+	}
+
+	if part_err != nil {
+		panic(part_err)
+	}
+
+	t0 := time.Now()
+	funcs[day-1][part-1]()
+	t1 := time.Now()
+
+	t := t1.Sub(t0)
+
+	fmt.Printf("Took: %.6f s", t.Seconds())
 }
